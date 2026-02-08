58808d6c5789dc2bc75b2bc89a0ab11c47b146ff
Asia Haslam <haslamwebsites@gmail.com>
2026-02-08 15:50:55 +0200

    implement simple fasta parser

--- viral-sequence-classifier/src/main/java/com/asiahaslam/viralclassifier/sequences/FastaParser.java
+++ viral-sequence-classifier/src/main/java/com/asiahaslam/viralclassifier/sequences/FastaParser.java
@@ -1,4 +1,32 @@
 package com.asiahaslam.viralclassifier.sequences;
 
+import java.io.*;
+import java.util.Scanner;
+
+// initial basic logic from https://rosettacode.org/wiki/FASTA_format
+// TODO: add logic for dealing with different kinds of formatting in files
+// TODO: instead of printing to console, create string objects from parsed FASTA files
+
 public class FastaParser {
+    public static void main(String[] args) throws FileNotFoundException {
+        boolean first = true;
+
+        try (Scanner sc = new Scanner(new File("src/main/java/com/asiahaslam/viralclassifier/sequences/test.fasta"))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                if (line.charAt(0) == '>') {
+                    if (first) {
+                        first = false;
+                    }
+                    else {
+                        System.out.println();
+                    }
+                }
+                else {
+                    System.out.print(line);
+                }
+            }
+        }
+        System.out.println();
+    }
 }
