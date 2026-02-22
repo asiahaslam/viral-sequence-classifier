5ac8e90895f330255f9462259b5fe313d5bfc71a
Asia Haslam <haslamwebsites@gmail.com>
2026-02-22 19:15:55 +0200

    add method to create scoring matrix

--- viral-sequence-classifier/src/main/java/com/asiahaslam/viralclassifier/algorithms/SmithWatermanAligner.java
+++ viral-sequence-classifier/src/main/java/com/asiahaslam/viralclassifier/algorithms/SmithWatermanAligner.java
@@ -20,6 +20,59 @@ public class SmithWatermanAligner {
         this.scoringMatrix = scoringMatrix1;
     }
 
+    public AlignmentResult align(String sequence1, String sequence2) {
+        if (sequence1 == null || sequence2 == null || sequence1.isEmpty() || sequence2.isEmpty()) {
+            return new AlignmentResult(0.0, 0.0);
+        }
+
+        // convert sequences to uppercase
+        sequence1 = sequence1.toUpperCase();
+        sequence2 = sequence2.toUpperCase();
+
+        // create scoring matrix
+        double[][] scoreMatrix = createScoringMatrix(sequence1, sequence2);
+
+        // find position of max score
+
+        // calculate normalized score
+
+        // traceback to find actual alignment
+    }
+
+    private double[][] createScoringMatrix(String seq1, String seq2) {
+        int rows = seq1.length() + 1;
+        int cols = seq2.length() + 1;
+
+        // create matrix
+        double[][] matrix = new double[rows][cols];
+
+        // initialize first row and column to 0 (always first step for Smith-Waterman scoring matrix)
+        for (int i = 0; i < rows; i++) {
+            matrix[i][0] = 0;
+        }
+        for (int j = 0; j < cols; j++) {
+            matrix[0][j] = 0;
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                /* find the corresponding characters for this row and column of the matrix
+                from the two sequences */
+                char char1 = seq1.charAt(i - 1);
+                char char2 = seq2.charAt(j - 1);
+
+                // calculate scores for the 3 possible operations
+                double match = matrix[i - 1][j - 1] + scoringMatrix.getScore(char1, char2);
+                double delete = matrix[i - 1][j] + scoringMatrix.getGapPenalty();
+                double insert = matrix[i][j - 1] + scoringMatrix.getGapPenalty();
+
+                // take maximum of the 3 operations above or 0 if they are all negative
+                matrix[i][j] = Math.max(0, Math.max(match, Math.max(delete, insert)));
+            }
+        }
+        return matrix;
+    }
+
     // helper class to store position and max value in matrix
     private static class MaxScorePosition {
         final double score;
