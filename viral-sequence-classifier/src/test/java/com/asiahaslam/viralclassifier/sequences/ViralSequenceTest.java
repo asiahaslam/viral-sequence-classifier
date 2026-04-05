675f1f245567d52d6224b10ae100918fedd54b9c
Asia Haslam <haslamwebsites@gmail.com>
2026-04-05 15:10:06 +0300

    implement ViralSequenceTest

--- viral-sequence-classifier/src/test/java/com/asiahaslam/viralclassifier/sequences/ViralSequenceTest.java
+++ viral-sequence-classifier/src/test/java/com/asiahaslam/viralclassifier/sequences/ViralSequenceTest.java
@@ -1,4 +1,28 @@
 package com.asiahaslam.viralclassifier.sequences;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ViralSequenceTest {
+
+    @Test
+    void testBasicCreation() {
+        ViralSequence seq = new ViralSequence("test", "ATCG", "TestVirus");
+        assertEquals("test", seq.getName());
+        assertEquals("ATCG", seq.getSequence());
+        assertEquals("TestVirus", seq.getVirusFamily());
+        assertEquals(4, seq.getLength());
+    }
+
+    @Test
+    void testCaseNormalization() {
+        ViralSequence seq = new ViralSequence("test", "atcg", "TestVirus");
+        assertEquals("ATCG", seq.getSequence());
+    }
+
+    @Test
+    void testEmptySequence() {
+        ViralSequence seq = new ViralSequence("empty", "", "TestVirus");
+        assertEquals(0, seq.getLength());
+    }
 }
